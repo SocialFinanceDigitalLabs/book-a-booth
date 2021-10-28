aa89e4c3515279a7180ada219f4da131c324cef3
Kaj Siebert <kaj@k-si.com>
2021-10-28 19:57:20 +0100

    Import MS Demo app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
+// If you need to support IE11 uncomment the imports below
+//import "react-app-polyfill/ie11";
+//import "core-js/stable"; 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider } from '@material-ui/core/styles';
+import { theme } from "./styles/theme";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// MSAL imports
+import { PublicClientApplication, EventType } from "@azure/msal-browser";
+import { msalConfig } from "./authConfig";
+
+export const msalInstance = new PublicClientApplication(msalConfig);
+
+// Default to using the first account if no account is active on page load
+if (!msalInstance.getActiveAccount() && msalInstance.getAllAccounts().length > 0) {
+  // Account selection logic is app dependent. Adjust as needed for different use cases.
+  msalInstance.setActiveAccount(msalInstance.getAllAccounts()[0]);
+}
+
+// Optional - This will update account state if a user signs in from another tab or window
+msalInstance.enableAccountStorageEvents();
+
+msalInstance.addEventCallback((event) => {
+  if (event.eventType === EventType.LOGIN_SUCCESS && event.payload.account) {
+    const account = event.payload.account;
+    msalInstance.setActiveAccount(account);
+  }
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Router>
+            <ThemeProvider theme={theme}>
+                <App pca={msalInstance} />
+            </ThemeProvider>
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
